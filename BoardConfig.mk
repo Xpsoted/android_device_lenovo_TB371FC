7c8dd6215a7c21d170a8c91658a4203649024605
skdushow <wangguanzhiabcd@126.com>
2024-02-03 22:42:03 +0800

    TB371FC:Add kernel header from qcom-caf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,7 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 androidboot.selinux=permissive
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+TARGET_KERNEL_SOURCE := $(DEVICE_PATH)/kernel-header
 
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
