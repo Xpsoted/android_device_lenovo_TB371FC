93e1bdfcab92f603566cfa6fa2071e140d72e1c5
skdushow <wangguanzhiabcd@126.com>
2024-02-04 12:57:19 +0800

    TB371FC:Add:Device Type:tablet

--- lineage_TB371FC.mk
+++ lineage_TB371FC.mk
@@ -4,22 +4,10 @@ DEVICE_PATH := device/lenovo/TB371FC
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Configure full_base_telephony.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Configure virtual_ab compression.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
-
-# Configure emulated_storage.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
-# Configure updatable_apex.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-# Configure developer_gsi_keys.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit common LineageOS configurations
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -30,5 +18,5 @@ PRODUCT_MANUFACTURER := lenovo
 PRODUCT_MODEL := TB371FC
 PRODUCT_NAME := lineage_TB371FC
 
-PRODUCT_CHARACTERISTICS := nosdcard
-
+PRODUCT_CHARACTERISTICS := tablet
+TARGET_BOOT_ANIMATION_RES := 1440
